13b6249cd11d04b9b670fe8ef9070fd1dc6ee6f0
Hao Men [xxx][==========/ <hao.men@gmail.com>
2019-10-27 13:21:47 -0400

    udpate to include QNetwork defined by pytorch

--- lunarlander_agent.py
+++ lunarlander_agent.py
@@ -1,8 +1,24 @@
 import random
 import numpy as np
 import torch
-from model import QNetwork
+import torch.nn as nn
+import torch.nn.functional as func
 
+class TorchQNetwork(nn.Module):
+    def __init__(self, state_size, action_size, random_seed, layer1_units = 64, layer2_units = 64):
+        '''Initialize 3 layers of linear NN '''
+        super(TorchQNetwork, self).__init__()
+        self.__seed = torch.manual_seed(random_seed)
+        self.__layer1 = nn.Linear(state_size, layer1_units)
+        self.__layer2 = nn.Linear(layer1_units, layer2_units)
+        self.__layer3 = nn.Linear(layer2_units, action_size)
+
+    def forward(self, state):
+        '''Implement forward method to provide action value based on state input'''
+        x = func.relu(self.__layer1(state))
+        x = func.relu(self.__layer2(x))
+        return self.__layer3(x)
+ 
 class Lunarlander():
     '''Lunarlander agent, include methods of step, act, Q-learn, update'''
     def __init__(self, state_size, action_size, random_seed):
@@ -22,7 +38,7 @@ class Lunarlander():
 
     # Methods to create Q network
     def create_q_network(self):
-        self.__q_network = QNetwork(self.__state_size, self.__action_size, self.__random_seed)
+        self.__q_network = TorchQNetwork(self.__state_size, self.__action_size, self.__random_seed)
 
     # Methods create training method
 
